5bdc3b7cb229d666a6d5324971d0d69208429024
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-06 16:27:52 +0100

    updated controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,7 @@ exports.registerUser = async (req, res) => {
 		user.password = await bcrypt.hash(password, salt);
 		await user.save();
 		const payload = { user: { id: user.id } };
+
 		jwt.sign(
 			payload,
 			config.get('jwtSecret'),
